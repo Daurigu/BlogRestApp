ea84fb393705ec0ba455ec44e802e340386d0fa2
daurigu <daurigu@outlook.com>
2021-01-22 17:16:47 -0500

    Secure update

--- blogging/blogging/settings.py
+++ blogging/blogging/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ed&pl^^93%+qb#-urzy^(h89-&fjx6_)um^b1^78wm20x9@uzs'
+with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
